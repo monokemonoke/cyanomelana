5905f86f852564e7c799198d7d498cf71d77096e
monokemonoke <kawabataryuhei@gmail.com>
2023-04-26 02:00:23 +0900

    add read_previous_line

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,60 @@
 use std::{
+    error::Error,
     fs::{self, File},
     io::{BufReader, Read, Seek, SeekFrom},
 };
 
-fn read_pdf(name: &String) {
-    const BUF_SIZE: usize = 1;
-    let file = File::open(name).unwrap();
-
-    let mut reader = BufReader::new(file);
-    reader.seek(SeekFrom::End(-1)).unwrap();
-
-    let mut buf = [0; BUF_SIZE];
-    for _ in 0..2 {
-        let mut line = String::new();
-        loop {
-            reader.read(&mut buf).unwrap();
-            if let Err(_) = String::from_utf8(buf.to_vec()) {
-                break;
+fn read_previous_line<R>(reader: &mut BufReader<R>) -> Result<String, Box<dyn Error>>
+where
+    R: Read + Seek,
+{
+    let mut line = String::new();
+    loop {
+        let mut buf = [0; 1];
+        reader.read(&mut buf).unwrap();
+        let c = match String::from_utf8(buf.to_vec()) {
+            Err(_) => {
+                reader.seek(SeekFrom::Current(-2)).unwrap();
+                return Ok(line);
             }
-            if let Ok(c) = String::from_utf8(buf.to_vec()) {
-                if c == "\n" {
-                    break;
-                }
-                line = format!("{}{}", c, line);
+            Ok(c) => c,
+        };
+
+        match c.as_str() {
+            "\n" => {
+                reader.seek(SeekFrom::Current(-2)).unwrap();
+                return Ok(line);
             }
-            reader.seek(SeekFrom::Current(-2)).unwrap();
+            _ => (),
         }
-        dbg!(name, line);
+        line = format!("{}{}", c, line);
         reader.seek(SeekFrom::Current(-2)).unwrap();
     }
 }
 
+fn read_pdf(name: &String) {
+    let file = File::open(name).unwrap();
+
+    let mut reader = BufReader::new(file);
+    reader.seek(SeekFrom::End(-2)).unwrap();
+
+    for _ in 0..2 {
+        let line = read_previous_line(&mut reader).unwrap();
+        dbg!(line);
+    }
+}
+
 fn main() {
-    let _: Vec<_> = fs::read_dir(".")
+    let path = "./resources";
+    let _: Vec<_> = fs::read_dir(path)
         .unwrap()
         .into_iter()
         .map(|v| v.unwrap().file_name().to_str().unwrap().to_string())
+        .map(|v| {
+            dbg!(&v);
+            v
+        })
         .filter(|v| v.contains(".pdf"))
-        .map(|v| read_pdf(&v))
+        .map(|v| read_pdf(&format!("{}/{}", path, v)))
         .collect();
 }
