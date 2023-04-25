7fa598af6318045be3ce7bf884c525d9c955c08b
monokemonoke <kawabataryuhei@gmail.com>
2023-04-26 01:35:34 +0900

    add: read eof marker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+use std::{
+    fs::{self, File},
+    io::{BufReader, Read, Seek, SeekFrom},
+};
+
+fn read_pdf(name: &String) {
+    const BUF_SIZE: usize = 1;
+    let file = File::open(name).unwrap();
+
+    let mut reader = BufReader::new(file);
+    reader.seek(SeekFrom::End(-1)).unwrap();
+
+    let mut buf = [0; BUF_SIZE];
+    for _ in 0..2 {
+        let mut line = String::new();
+        loop {
+            reader.read(&mut buf).unwrap();
+            if let Err(_) = String::from_utf8(buf.to_vec()) {
+                break;
+            }
+            if let Ok(c) = String::from_utf8(buf.to_vec()) {
+                if c == "\n" {
+                    break;
+                }
+                line = format!("{}{}", c, line);
+            }
+            reader.seek(SeekFrom::Current(-2)).unwrap();
+        }
+        dbg!(name, line);
+        reader.seek(SeekFrom::Current(-2)).unwrap();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _: Vec<_> = fs::read_dir(".")
+        .unwrap()
+        .into_iter()
+        .map(|v| v.unwrap().file_name().to_str().unwrap().to_string())
+        .filter(|v| v.contains(".pdf"))
+        .map(|v| read_pdf(&v))
+        .collect();
 }
