f94daa08b6f2e5d33106059bae25d63f9140f3af
monokemonoke <kawabataryuhei@gmail.com>
2023-04-28 00:14:08 +0900

    parse xref table

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{self, File},
-    io::{BufReader, Error, ErrorKind, Read, Seek, SeekFrom},
+    io::{BufRead, BufReader, Error, ErrorKind, Read, Seek, SeekFrom},
 };
 
 fn read_previous_line<R>(reader: &mut BufReader<R>) -> Result<String, Error>
@@ -42,10 +42,34 @@ fn read_pdf(name: &String) {
     let mut reader = BufReader::new(file);
     reader.seek(SeekFrom::End(-2)).unwrap();
     if let Err(_) = check_eof_with_limit(&mut reader, 16) {
-        println!("failed to check eof skipped...");
         return;
     }
-    println!("{}", read_previous_line(&mut reader).unwrap());
+
+    let xref_byte = read_previous_line(&mut reader).unwrap();
+    let xref_byte: u64 = xref_byte.parse().unwrap();
+
+    reader.seek(SeekFrom::Start(xref_byte)).unwrap();
+    let mut buf = String::new();
+
+    reader.read_line(&mut buf).unwrap();
+    if !buf.starts_with("xref") {
+        return;
+    }
+    buf.clear();
+    reader.read_line(&mut buf).unwrap();
+    let num_of_objects_str = buf.split(' ').last().unwrap().trim_end();
+    let num_of_objects: u64 = num_of_objects_str.parse().unwrap();
+    println!("{}", num_of_objects);
+    for _ in 0..num_of_objects {
+        let mut buf = String::new();
+        reader.read_line(&mut buf).unwrap();
+        let parts: Vec<&str> = buf.split_whitespace().collect();
+        let object_id = parts[0];
+        let generation = parts[1];
+        let byte = parts[2];
+
+        println!("{} {} {}", object_id, generation, byte);
+    }
 }
 
 fn main() {
